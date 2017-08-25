6e70203ad61a733f1ac29d8d203bf66835895308
acarrab <acarrabba6@gmail.com>
2017-08-24 22:09:54 -0400

    adding base structure of react website

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import { render } from 'react-dom';
+
+const App = () => { return (<h1>Hello World, yet again</h1>) };
+
+render(<App />, document.getElementById('root'));
