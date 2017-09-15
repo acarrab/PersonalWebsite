eca6005ed10cc3213f2966a3e4a0dc1fc37611e1
Angelo Carrabba <acarrab@34.181.21.198.tigernet.wifi.dyn.clemson.edu>
2017-09-15 10:33:08 -0400

    Saying what is going on with website

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,11 @@ import React from "react";
 export default class Home extends React.Component {
     public render() {
         return (
-            <h2>Welcome</h2>
+            <div>
+                <h2>Welcome</h2>
+                <p>Currently under development.</p>
+                <p>Making node representation of website with canvas in order to represent user travel grapically with this in the background of the website</p>
+            </div>
         );
     }
 }
